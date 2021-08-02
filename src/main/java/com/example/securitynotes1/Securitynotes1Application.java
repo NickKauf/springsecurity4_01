f5c8280c677002bf9f29015b2d66905d833a9829
nickkauf <nkauf87@gmail.com>
2021-08-02 13:09:28 -0400

    first commit

---
+++ src/main/java/com/example/securitynotes1/Securitynotes1Application.java
@@ -0,0 +1,13 @@
+package com.example.securitynotes1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securitynotes1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Securitynotes1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/securitynotes1/Securitynotes1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitynotes1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Securitynotes1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
